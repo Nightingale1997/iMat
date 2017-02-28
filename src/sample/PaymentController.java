e20eb327c23db6242d0b9fbaa6d2b2a4f8029692
Filip Lindahl <gysztaf@gmail.com>
2017-02-28 22:36:56 +0100

    Gjorde en controller till, tog bort lite onödigt i en .fxml

---
+++ src/sample/PaymentController.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * Created by flirre on 2/28/17.
+ */
+public class PaymentController {
+    @FXML
+    private Button yesButton, noButton;
+    
+    @FXML
+    private void onClick(){
+        System.out.println("helo");
+    }
+}
