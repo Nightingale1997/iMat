178203e59a1e8e7d6dc0e9ba7b684f3014fe399c
Elina Olsson <elinao@student.chalmers.se>
2017-03-07 17:21:25 +0100

    Update

--- src/sample/PayDeliveryLoggedInController.java
+++ src/sample/PayDeliveryLoggedInController.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 
 /**
  * Created by Elina Olsson on 2017-03-07.
@@ -10,7 +11,13 @@ public class PayDeliveryLoggedInController {
     static Controller controller = Controller.getThisInstance();
 
     @FXML
+    ChoiceBox deliveryDay, deliveryTime;
+
+    @FXML
     private void loadConfirmation() {
         controller.changeMainTo("scenes/components/confirmation.fxml");
     }
+
+    
+
 }
