a69dd4178c720ebd0c68b73e8be1a7631ab6b7b8
Nightingale1997 <kevin.solovjov@gmail.com>
2017-03-08 09:57:55 +0100

    Added homepage on first run

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,21 +7,41 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.text.*;
 
+import java.io.File;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        
 
         // load the  font.
         Font.loadFont(getClass().getResourceAsStream("css/OpenSans.ttf"), 14);
 
-        //Parent root = FXMLLoader.load(getClass().getResource("scenes/payinfo.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("scenes/sample2.fxml"));
-        primaryStage.setTitle("iMat - Handla Enkelt!");
-        primaryStage.setScene(new Scene(root, 1280, 720));
-        primaryStage.show();
-        //primaryStage.setFullScreen(true);
+
+        File file = new File("firstrun.txt");
+        boolean firstRun = false;
+        if(!file.exists()){
+            firstRun = true;
+            file.createNewFile();
+
+            //Parent root = FXMLLoader.load(getClass().getResource("scenes/payinfo.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("scenes/sample3.fxml"));
+            primaryStage.setTitle("iMat - Handla Enkelt!");
+            primaryStage.setScene(new Scene(root, 1280, 720));
+            primaryStage.show();
+            //primaryStage.setFullScreen(true);
+        }
+        else{
+            //Parent root = FXMLLoader.load(getClass().getResource("scenes/payinfo.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("scenes/sample2.fxml"));
+            primaryStage.setTitle("iMat - Handla Enkelt!");
+            primaryStage.setScene(new Scene(root, 1280, 720));
+            primaryStage.show();
+            //primaryStage.setFullScreen(true);
+        }
+
+
+
     }
 
 
