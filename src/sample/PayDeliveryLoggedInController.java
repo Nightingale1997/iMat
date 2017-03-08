a03eefdb1a4d5452b06831bd9a2f4c4cc9a7b4ba
Elina Olsson <elinao@student.chalmers.se>
2017-03-08 09:51:01 +0100

    Försöker få ChoiceBox att fungera

--- src/sample/PayDeliveryLoggedInController.java
+++ src/sample/PayDeliveryLoggedInController.java
@@ -1,7 +1,13 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+
+import java.net.URL;
+import java.util.Observable;
+import java.util.ResourceBundle;
 
 /**
  * Created by Elina Olsson on 2017-03-07.
@@ -10,14 +16,23 @@ public class PayDeliveryLoggedInController {
 
     static Controller controller = Controller.getThisInstance();
 
-    @FXML
-    ChoiceBox deliveryDay, deliveryTime;
+    ObservableList<String> dayChoices = FXCollections.observableArrayList("Måndag", "Tisdag");
+
+    //@FXML
+    //private ChoiceBox deliveryDay, deliveryTime;
 
     @FXML
     private void loadConfirmation() {
         controller.changeMainTo("scenes/components/confirmation.fxml");
     }
 
-    
+    @FXML
+    private ChoiceBox deliveryDay = new ChoiceBox();
 
+
+    @FXML
+    private void initialize(URL location, ResourceBundle resources) {
+        deliveryDay.setItems(dayChoices);
+        //deliveryDay.getItems().addAll("Måndag", "Tisdag");
+    }
 }
